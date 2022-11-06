5ab69bdb4d2cb66295036641ae0bf55d8cfc60e0
Novaisn <nelsonnovais@ipvc.pt>
2022-11-06 00:23:46 +0000

    403

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -88,12 +88,132 @@ export const AuthProvider = ({children}) => {
         console.log(pag);
     }
 
+    const addCliente = async () => {
+        console.log("Entrei")
+        nome_cliente = ""
+        nif_cliente = 190031107
+        pais_cliente = "Portugal"
+        endereco_cliente = ""
+        codigopostal_cliente
+        regiao_cliente 
+        cidade_cliente
+        email_cliente 
+        website_cliente 
+        tlm_cliente
+        tlf_cliente
+        fax_cliente 
+        preferencial_nome_cliente
+        preferencial_email_cliente
+        preferencial_tlm_cliente 
+        preferencial_tlf_cliente 
+        pagamento_cliente 
+        vencimento_cliente 
+        desconto_cliente 
+        flagContaGeral 
+        codigo_interno_cliente 
+        let _token = userToken;
+        let opcao = 2;
+        axios.post(`${BASE_URL}/api/tabelas/clientes`, {
+            _token,
+            opcao,
+            nome_cliente,
+        nif_cliente,
+        pais_cliente,
+        endereco_cliente,
+        codigopostal_cliente,
+        regiao_cliente,
+        cidade_cliente,
+        email_cliente,
+        website_cliente,
+        tlm_cliente,
+        tlf_cliente,
+        fax_cliente,
+        preferencial_nome_cliente,
+        preferencial_email_cliente,
+        preferencial_tlm_cliente,
+        preferencial_tlf_cliente,
+        pagamento_cliente,
+        vencimento_cliente,
+        desconto_cliente,
+        flagContaGeral,
+        codigo_interno_cliente,
+        })
+        .then(async res => {
+            console.log(res.data)
+            return res.data
+        }).catch(e =>{
+            console.log(`Erro: ${e}`);
+            setIsLoading(false)
+        });
+    }
+
+    const addOrcamentos = async () => {
+            console.log("Entrei");
+            cliente = 1;
+            serie = 1;
+            numero = 3;
+            data= "05/11/2022";
+            validade = "12/12/2022";
+            referencia = "132165";
+            vencimento = 1000;
+            moeda = 1;
+            desconto = 0;
+            observacoes = "";
+            artigo= 3;
+            descricao="gota"; 
+            qtd=1; 
+            preco=1,626; 
+            imposto=1; 
+            motivo=0 
+            desconto=0
+            retencao=0
+            Linha = [artigo,descricao, qtd, preco, imposto, motivo, desconto, retencao]
+            Linhas = [Linha];
+            finalizarDocumento=1;
+            setIsLoading(true);
+            let _token = userToken;
+            let opcao = 2;
+            //console.log(userToken);
+            axios.post(`${BASE_URL}/api/orcamentos/orcamentos`, {
+                _token,
+                opcao,
+                cliente,
+                serie,
+                numero,
+                data,
+                validade,
+                referencia,
+                vencimento,
+                moeda,
+                desconto,
+                observacoes,
+                artigo,
+                descricao, 
+                qtd, 
+                preco, 
+                imposto, 
+                motivo, 
+                desconto,
+                retencao,
+                Linhas,
+                finalizarDocumento,
+            })
+            .then(async res => {
+                console.log(res.data)
+                return res.data
+            }).catch(e =>{
+                console.log(`Erro: ${e}`);
+                setIsLoading(false)
+            });
+
+    }
+
     useEffect(() => {
         isLoggedIn();
     }, []);
 
     return(
-        <AuthContext.Provider value={{login, logout, getOrcamentos,isLoading, userToken}}>
+        <AuthContext.Provider value={{login, logout, getOrcamentos,addOrcamentos,addCliente,isLoading, userToken}}>
             {children}
         </AuthContext.Provider>
     );
--- src/Screens/Orcamentos/CriarOrcamento.js
+++ src/Screens/Orcamentos/CriarOrcamento.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { useState, useEffect,useContext } from 'react';
 import { Button, StyleSheet, Text,Touchable, TouchableNativeFeedback, TouchableOpacity, View } from 'react-native';
-
+import { AuthContext } from "../../Context/AuthContext";
 
 export default function CriarOrcamento({navigation}) {
    
-     
+    const {addOrcamentos} = useContext(AuthContext);
+    const {addCliente} = useContext(AuthContext);
+
     return (
       <View style={styles.container}>
-          
+                <TouchableOpacity onPress={() => addCliente()}><Text>Marega
+                    
+                    </Text></TouchableOpacity>
+         
        
         
   
