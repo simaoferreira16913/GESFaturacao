b349798939aef723e44a30b53c5a554ad8552347
Novaisn <nelsonnovais@ipvc.pt>
2022-12-01 16:17:26 +0000

    A fazer form para criar cliente

--- src/Screens/Clientes/CriarCliente.js
+++ src/Screens/Clientes/CriarCliente.js
@@ -1,73 +1,62 @@
 import React from "react";
 import { useState, useEffect,useContext } from 'react';
 import { Button, StyleSheet, Text,TextInput,Touchable, Alert, 
-  TouchableNativeFeedback, TouchableOpacity, View } from 'react-native';
+  TouchableNativeFeedback, TouchableOpacity, View , ScrollView} from 'react-native';
 import { AuthContext } from "../../Context/AuthContext";
+import { useForm } from "react-hook-form";
+
 
 export default function CriarCliente({navigation}) {
    
-
+  const { control,register, handleSubmit, watch, formState: { errors } } = useForm();
     
       this.textInput = React.createRef();
     
-    const {addOrcamentos} = useContext(AuthContext);
-    const {addCliente} = useContext(AuthContext);
 
+    const {addCliente} = useContext(AuthContext);
+     
     const [textNif,setTextNif] = useState()
-    const [nif, setNif] = useState(null)
-    const [nome, setNome] = useState(null)
-    const [codInterno, setCodInterno] = useState(null)
-    const [endereco, setEnderenco] = useState(null)
-    const [codPostal, setCodPostal] = useState(null)
-    const [localidade, setLocalidade] = useState(null)
-    const [distrito, setDistrito] = useState(null)
-    const [cidade, setCidade] = useState(null)
-    const [pais, setPais] = useState(null)
-    const [email, setEmail] = useState(null)
-    const [website, setWebsite] = useState(null)
-    const [tlm, setTlm] = useState(null)
-    const [tlf, setTlf] = useState(null)
-    const [fax, setFax] = useState(null)
-    const [preferencial_nome, setPreferencial_nome] = useState(null)
-    const [preferencial_email, setPreferencial_email] = useState(null)
-    const [preferencial_tlm, setPreferencial_tlm] = useState(null)
-    const [preferencial_tlf, setPreferencial_tlf] = useState(null)
-    const [pagamento, setPagamento] = useState(null)
-    const [vencimento, setVencimento] = useState(null)
-    const [desconto, setDesconto] = useState(null)
-    const [flagContaGeral, setFlagContaGeral] = useState(null)
-    const [codigo_interno, setCodigoInterno] = useState(null);
+    const [nif, setNif] = useState()
+    const [nome, setNome] = useState()
+    const [endereco, setEnderenco] = useState()
+    const [codPostal, setCodPostal] = useState()
+    const [regiao, setRegiao] = useState()
+   
+    const [cidade, setCidade] = useState()
+    const [pais, setPais] = useState()
+    const [email, setEmail] = useState()
+    const [website, setWebsite] = useState()
+    const [tlm, setTlm] = useState()
+    const [tlf, setTlf] = useState()
+    const [fax, setFax] = useState()
+    const [vencimento, setVencimento] = useState()
+    const [desconto, setDesconto] = useState()
+    
+   /* const dadosCliente={Nome:"nome", Nif:"193047663", Pais:"PT",Endereco:"endereco",CodigoPostal:4755-123,
+    Regiao:0, Cidade:0,Email:"teste@teste.com",Website:"teste@teste.com",Telemovel:968000000,Telefone:252000000
+    ,Fax:252000001, Vencimento:0, Desconto:0};
+    const dadosCli={Nome:nome, Nif:textNif, Pais:pais,Endereco:endereco,CodigoPostal:codPostal,
+                Regiao:regiao, Cidade:cidade,Email:email,Website:website,Telemovel:tlm,Telefone:tlf
+                ,Fax:fax, Vencimento:0, Desconto:0};*/
 
     const {criarCliente} = useContext(AuthContext);
+     
 
-    function onChanged(text) {
-      let newText='';
-      let numbers = '0123456789';
 
-      for (var i=0; i< text.lenght; i++){
-        if(numbers.indexOf(text[i]) > -1){
-          newText = newText + text[i];
-          console.log(newText)
-        }
-        else {
-          Alert.alert("Digite apenas números")
-          
-        }
-      }
-      console.log(newText)
-      setNif(newText)
-      
+      submitcliente = () => {
+        criarCliente(this.state.newCliente)
     }
-
 
    function validateInputs(text, type) {
       let numreg = /^[0-9]+$/;
-      console.log("e")
+      
         if (type == 'nif') {
           if (numreg.test(text)) {
             //ok
+            console.log(text)
             setTextNif(text)
-          } else {
+          } 
+          else {
             //nao ok
             text = null
             Alert.alert("Digite Apenas numeros")
@@ -75,29 +64,72 @@ export default function CriarCliente({navigation}) {
           } 
         
         }
+        if(type == 'codpostal'){
+          if (numreg.test(text)) {
+            //ok
+            setCodPostal(text)
+          } 
+          else {
+            //nao ok
+            text = null
+            Alert.alert("Digite Apenas numeros")
+            setCodPostal('')
+          }
+        }
+        if(type == 'tlm'){
+          if (numreg.test(text)) {
+            //ok
+            setTlm(text)
+          } 
+          else {
+            //nao ok
+            text = null
+            Alert.alert("Digite Apenas numeros")
+            setTlm('')
+          }
+        }
+        if(type == 'tlf'){
+          if (numreg.test(text)) {
+            //ok
+            setTlf(text)
+          } 
+          else {
+            //nao ok
+            text = null
+            Alert.alert("Digite Apenas numeros")
+            setTlf('')
+          }
+        }
+        if(type == 'fax'){
+          if (numreg.test(text)) {
+            //ok
+            setFax(text)
+          } 
+          else {
+            //nao ok
+            text = null
+            Alert.alert("Digite Apenas numeros")
+            setTlf('')
+          }
+        }
+        if(type == 'vencimento'){
+          if (numreg.test(text)) {
+            //ok
+            setVencimento(text)
+          } 
+          else {
+            //nao ok
+            text = null
+            Alert.alert("Digite Apenas numeros")
+            setVencimento('')
+          }
+        }
     }
+    
     return (
-      <View style={styles.container}>
-          <View style={styles.wrapper}>
-            <Text>Nif</Text>
-          <TextInput
-            style={styles.input}
-            value={textNif}
-            onChangeText={(text) => validateInputs(text, 'nif')}
-            placeholder="NIF"
-            keyboardType = 'numeric'
-          />
-          
+       <form>
 
-        <Button
-          title="Criar Cliente" color='#d0933f'
-          onPress={() => {addCliente()}}
-        />
-        </View>
-      
-        
-  
-      </View>
+       </form>
       
     );
   
