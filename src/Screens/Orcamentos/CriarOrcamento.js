5f5c0783f9702726ed8e20d9a600a8b25b12fec8
Novaisn <nelsonnovais@ipvc.pt>
2023-01-20 22:46:16 +0000

    o array do linhas já está a dar

--- src/Screens/Orcamentos/CriarOrcamento.js
+++ src/Screens/Orcamentos/CriarOrcamento.js
@@ -1,45 +1,63 @@
 import React from "react";
 import { useState, useEffect,useContext } from 'react';
-import { Button, StyleSheet, Text,Touchable, TouchableNativeFeedback, TouchableOpacity, View,FlatList } from 'react-native';
+import { Button, StyleSheet, Text,Touchable, TouchableNativeFeedback, TouchableOpacity, View,FlatList, TextInput } from 'react-native';
 import { AuthContext } from "../../Context/AuthContext";
 import {Picker} from '@react-native-picker/picker';
 import { BASE_URL } from '../../config';
 import DatePicker from 'react-native-date-picker'
-import {yupResolver} from '@hookform/resolvers/yup'
-import * as yup from 'yup';
+import {useForm} from 'react-hook-form';
+//import {yupResolver} from '@hookform/resolvers/yup'
+//import * as yup from 'yup';
 
+/*const schema = yup.object().shape({
+    //artigo: yup.required("Selecione um artigo"),
+    quantidade: yup.number().required("Insira a quantidade")
+      .positive("A quantidade deve ser positiva")
+        .integer("A quantidade deve ser inteira"),
+    preco: yup.number().required("Inserira o preco")
+    .positive("O preço deve ser positivo").min(0.01,"Insira um preço válido")
+});*/
 
 
 export default function CriarOrcamento({navigation}){
-    const {addOrcamentos} = useContext(AuthContext);
-    const {insertCliente} = useContext(AuthContext);
-    const {deletecliente} = useContext(AuthContext);
-    const {criarCliente} = useContext(AuthContext);
+    
     //const {getOrcamentos} = useContext(AuthContext);
     const {getClientes} = useContext(AuthContext);
     const {getclienteID} = useContext(AuthContext)
     const {getArtigos} = useContext(AuthContext);
     var coisa;
     
-
+  /*const {register, handleSubmit, errors} = useForm({
+    resolver: yupResolver(schema)
+  });*/
   const [dadosClientes,setDadosClientes] = useState([]);
   const [dadosArtigos,setDadosArtigos] = useState([]);
   const [cliente, setCliente] = useState();
   const [linhas, setLinhas] = useState([]);
   const [datei, setDatei] = useState(null)
-  const [open, setOpen] = useState(false)
-  
-  
-  getArtigos().then((res)=>{
-    setDadosArtigos(res.data.aaData)
+  const [open, setOpen] = useState(false);
+  const [artigo, setArtigo] = useState();
+  const [quantidade, setQuantidade] = useState();
+  const [preco, setPreco] = useState();
+
+  const onSubmit = (data) => {
+    setLinhas([...linhas, data]);
+  }
+
+  if(!dadosArtigos.length){
+    getArtigos().then((res)=>{
+      setDadosArtigos(res.data.aaData)
     
-  });
-  getClientes().then((res)=>{
-    //console.log(res.data)
-    setDadosClientes(res.data.aaData)
-    //console.log(dadosClientes)
-    //return res.data.aaData
-  })
+    });
+  }
+  if(!dadosClientes.length){
+    getClientes().then((res)=>{
+      console.log(res.data)
+      setDadosClientes(res.data.aaData)
+      //console.log(dadosClientes)
+      //return res.data.aaData
+    });
+  }
   /*if (dadosClientes.length === 0){
     ;
   }else{
@@ -59,24 +77,27 @@ export default function CriarOrcamento({navigation}){
   }*/
   
   
- 
+ console.log(linhas);
   
-  const [selectedId, setSelectedId] = useState(null);
+  const [selectedIdCliente, setSelectedIdCliente] = useState(null);
+  const [selectedIdArtigo, setSelectedIdArtigo] = useState(null);
 /* getclienteID(1).then(response => {
     
     setCliente(response.data.data)
     //console.log(response.data)
     console.log(cliente)
 });*/
-  if(selectedId != null){
+  /*if(selectedId != null){
     getclienteID(selectedId).then(response => {
     
       setCliente(response.data.data)
       //console.log(response.data)
-      console.log(cliente)
+      //console.log(cliente)
   });
+  }*/
+  if(selectedIdArtigo != null){
+    
   }
-  
     return (
       <View style={styles.container}>
             
@@ -89,7 +110,7 @@ export default function CriarOrcamento({navigation}){
             <Text style={styles.textfont}>   Novo Cliente</Text>
           </View>
         </TouchableNativeFeedback>
-        <Picker placeholder="Selecione um cliente" onValueChange={itemValue=>setSelectedId(itemValue)}>
+        <Picker placeholder="Selecione um cliente" selectedValue={selectedIdCliente} onValueChange={itemValue=>setSelectedIdCliente(itemValue)}>
           {dadosClientes.map(function(object,i){
       return <Picker.Item label={object[2]} value={object[0]} key={i}/>;
     })}
@@ -110,12 +131,32 @@ export default function CriarOrcamento({navigation}){
           setOpen(false)
         }}
       />
-      <Picker placeholder="Selecione um Artigo" onValueChange={itemValue=>setSelectedId(itemValue)}>
+      <Picker placeholder="Selecione um Artigo"
+        selectedValue={selectedIdArtigo} onValueChange={itemValue=>setSelectedIdArtigo(itemValue)} >
           {dadosArtigos.map(function(object,i){
       return <Picker.Item label={object[1]} value={object[0]} key={i}/>;
     })}
         </Picker>
-        
+        {/* {errors.artigo && <Text>{errors.artigo.message}</Text>} */}
+        <TextInput
+          value={quantidade}
+          onChangeText={(text)=>setQuantidade(text)}
+          placeholder="Quantidade"
+          keyboardType="numeric"
+          // ref={register({name: "quantidade"})} 
+        />
+        {/* {errors.quantidade && <Text>{errors.quantidade.message}</Text>} */}
+        <TextInput
+          value={preco}
+          onChangeText={(text)=> setPreco(text)}
+          placeholder="Preço"
+          keyboardType="numeric"
+          // ref={register({name:"preco"})}
+          />
+          {/* {errors.preco && <Text>{errors.preco.message}</Text>} */}
+           <Button title="Adicionar" onPress={()=> setLinhas([...linhas,
+              selectedIdArtigo, quantidade, preco])}
+            /> 
       </View> 
      
   
